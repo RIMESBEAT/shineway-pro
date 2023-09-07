1be0f65aad24dd6e733f48548166f6fab06ce9d5
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2023-09-07 17:56:17 +0100

    faqs added

--- src/app/(users)/contact/page.jsx
+++ src/app/(users)/contact/page.jsx
@@ -1,9 +1,43 @@
 "use client";
-import React from 'react'
+import Accordion from '@/components/accordion/Accordion';
+import { faqsCompany, faqsProducts } from '@/global/faqs';
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        message: '',
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formData);
+
+        setFormData({
+          firstName: '',
+          lastName: '',
+          email: '',
+          phoneNumber: '',
+          message: '',
+        });
+
+      };
+
   return (
-    <main className=' '>
+    <main className='padding__x '>
       
      
 
@@ -14,43 +48,106 @@ const Contact = () => {
 </div>
 
 
-<form action="" className='justify-center flex-col flex items-center py-10 '>
-    <div className="block md:flex items-center justify-center gap-8 w-full">
-        <div className="">
-<label >First Name</label>
-<br />
-<input type="text" />
+<form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <div className="">
-        <label >Last Name</label>
-        <br />
-<input type="text" />
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
         </div>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="4"
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+<div className="my-10 pt-20">
+<h1 className='py-4 font-extrabold text-4xl'>FAQs</h1>
+
+<div className="block md:flex items-start  gap-8 ">
+<div className="flex-1 py-10">
+    <h1 className='font-bold text-lg'>FAQ&apos;s About Company</h1>
+    {
+faqsCompany?.map((item)=>(
+    <div className="py-4" key={item.id}>
+<Accordion
+        title={item.question}
+        content={<div className=""> 
+        <p>{item.answer}</p>
+         
+          </div>
+          
+        }
+      />
     </div>
-    <div className="block md:flex items-center justify-center gap-8">
-        <div className="flex-1">
-<label >Email Address</label>
-<br />
-<input type="text" />
-        </div>
-        <div className="flex-1">
-        <label >Last Name</label>
-        <br />
-<input type="text" />
-        </div>
+))
+    }
     </div>
-        <div className="">
-           <label > Message Us</label>
+    <div className="flex-1 py-10">
+<h1 className='font-bold text-lg '>FAQ&apos;s About Products</h1>
+    {
+faqsProducts?.map((item)=>(
+    <div className="py-4" key={item.id}>
+<Accordion
+        title={item.question}
+        content={<div className="bold"> 
+        <p>{item.answer}</p>
+         
+          </div>
+          
+        }
+      />
+    </div>
+))
+    }
+    </div>
 
-
-<br />
-           <textarea className='flex-1' />
-
-        </div>
-</form>
-
-<div className="">
-
+</div>
 </div>
      
         <div className="">
---
+++ src/global/faqs.js
@@ -0,0 +1,94 @@
+export const faqsProducts =[
+    {
+        id: 1,
+        question: "What is Shineway Healthcare all about?",
+        answer: "Shineway Healthcare is a is a pharmaceutical company that produces traditional Chinese Medicine (TMC) with about seven factories across China."
+    },
+    {
+        id: 2,
+        question: " Are shineway products supplement?",
+        answer: "No, Shineway products are not supplement but curative herbal."
+    },
+    {
+        id: 3,
+        question: "How effective are the products?",
+        answer: "Very effect because of its Nano and peptide bond that allows it penetrate fast from the walls of your stomach to our blood stream."
+    },
+    {
+        id: 4,
+        question: "Can these products be taken alongside other drugs?",
+        answer: "Yes, it can but should give a space of an hour before taking another drug"
+    },
+    {
+        id: 5,
+        question: "Can Shineway product generate cancer or ulcer?",
+        answer: "No."
+    },
+    
+    
+]
+export const faqsCompany =[
+   
+    {
+        id: 1,
+        question: "How do I become a member?",
+        answer: "By registering with the company with any of its starter's kits. (consumers pack, bronze, silver, gold or platinum)"
+    },
+    {
+        id: 2,
+        question: "After registration is up upgrade allow?",
+        answer: "Yes, you can upgrade to any level within 60 days from the day of your initial registration."
+    },
+    {
+        id: 3,
+        question: "Within the 60 day, how many times can one upgrade?",
+        answer: "Only once"
+    },
+    {
+        id: 4,
+        question: " After upgrading can I still collect the additional free products?",
+        answer: "Only once"
+    },
+    {
+        id: 5,
+        question: " After upgrading can I still collect the additional free products?",
+        answer: "No, you can't."
+    },
+    {
+        id: 6,
+        question: "  How to benefit from this company?",
+        answer: "We have 5 various bonus that allow you benefits from us"
+    },
+    {
+        id: 7,
+        question: "Can a none registered member benefit from the buy one get one free of every product?",
+        answer: "No, buy one get one free is meant for only registered member."
+    },
+    {
+        id: 8,
+        question: "Can I do any other network business as a registered member of Shineway?",
+        answer: "No, you are not permitted as it's against the code ethic of the company."
+    },
+    {
+        id: 9,
+        question: " Is this business transferable?",
+        answer: "Yes, it is."
+    },
+    {
+        id: 10,
+        question: " When do we pay our bonus?",
+        answer: "Weekly and Monthly"
+    },
+    {
+        id: 11,
+        question: "  Is there any charges or deduction from the bonuses?",
+        answer: "No, there is no charges only 10 % percent of all earnings are deducted and credited to E-coin."
+    },
+    
+    {
+        id: 12,
+        question: " Can a resisted member have more than one account with same name and bank Information?",
+        answer: "Yes, you can have more than one account."
+    },
+    
+]
