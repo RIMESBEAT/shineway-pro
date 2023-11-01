e2f53e8667b0d8c8c48beab2489e30ef7d53aded
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2023-11-01 13:10:34 +0000

    blog error fixed

--- src/app/(users)/blog/page.jsx
+++ src/app/(users)/blog/page.jsx
@@ -76,7 +76,7 @@ const handlePrevPage = () => {
 
   <div className="my-10 flex-[5]  grid grid-cols-1  sm:grid-cols-2 gap-4 rounded-md mb-10">
 {
-  postData?.map((item)=>(
+  posts?.map((item)=>(
    <div className=" shadow-lg " key={item._id}>
     <Link href={`blog/${item.slug.current}`} className=" " >
       <Article title={item.title} imgUrl={item.imgUrl} category={item.category} description={item.description} _createdAt={item._createdAt}/>
