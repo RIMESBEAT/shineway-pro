24df0ccce8512e3f16e69edf9c4b89ee1e479d8b
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2023-11-15 13:27:48 +0000

    Update env.js

--- env.js
+++ env.js
@@ -5,7 +5,7 @@ export const apiVersion =
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const useCdn =true
+export const useCdn =false
 
 export const client = createClient({
   apiVersion,
